cc6007da2497a17d213c79e2b1af79e7ca505a24
unknown <shayan.webdesigner@gmail.com>
2017-09-26 04:48:39 +0500

    Add utils and lifecycles with more functions

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -1,62 +1,84 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-
-
+import { StyleSheet, Text, View, ScrollView, TextInput, Button, TouchableOpacity} from 'react-native';
+import { capitalize } from '../../utils/helper';
+import FontAwesome, { Icons } from 'react-native-fontawesome';
 
 export default class Todo extends React.Component {
-
-  ComponentWillMount() {
-      console.log('ComponentWillMount');
-  }
   constructor() {
       super();
       this.state = {
-          todos: []
+          todos: [],
+          value: '',
       }
   }
+  
+  componentDidMount() {
+    Expo.Font.loadAsync({
+        'FontAwesome': require('../../node_modules/react-native-fontawesome/fontawesome-webfont.ttf'),
+    });
+  }
+  
+  componentWillMount(){
+      console.log('componentWillMount')
+  }
+
+  onChangeInput = (text) => {
+    console.log(text)
+    this.setState({
+      value: text
+    })
+  }
+
+    clearText(fieldName) {
+        this.refs[fieldName].setNativeProps({text: ''});
+    }
+    
+    addTodo = () => {
+    let todo = this.state.todos;
+    todo.push(this.state.value)
+    this.setState({
+        todos: todo
+    })    
+    this.clearText('textfield')
+    }
+    
+
+
   render() {
     return (
-      <View style={styleTodo.todoMain}>
+      <ScrollView style={styleTodo.todoMain} contentContainerStyle={styleTodo.todoContainer}>
         <TextInput
           style={styleTodo.textInput}
           placeholder="My Todo"
-          onChangeText={
-              this.state.todos.map((value, index) => {
-                  return <Text> {value} </Text>
-              })
-          }
+          ref="textfield"
+          onChangeText={this.onChangeInput.bind(this)}
         />
-        <Button
-            onPress={addTodo.bind(this)}
-            color="#333"
-            title="Add"
-        />
-      </View>
+        <TouchableOpacity style={styleTodo.addBtn} onPress={this.addTodo.bind(this)}>
+            <Text style={styleTodo.addBtnText}>
+              Add
+            </Text>
+          </TouchableOpacity>
+        {
+            this.state.todos.map((value, index, i) => {
+                return <Text style={styleTodo.item} key={index}>
+                {index + 1} -- {capitalize
+                (value)}
+                </Text>
+            })
+        }
+      </ScrollView>
     );
   }
 }
 
 
-todoApp = () => {
-    console.log("On Press");
-}
-onChangeInput = (text) => {
-    // console.log(text)
-    let todo = this.state.todos
-    todo.push(text)
-    this.setState({
-        value: text
-    });
-}
-
-addTodo = () => {
-    let todo = this.state.todos
-}
 
 const styleTodo = StyleSheet.create({
   todoMain: {
-     flex: 1,
-     alignItems: 'center',
+    flex: 1,
+  },
+  todoContainer: {
+    alignItems: 'center',
   },
   textInput: {
      height: 40,
@@ -69,5 +91,24 @@ const styleTodo = StyleSheet.create({
   },
   todoAdd: {
     width: 80,
+  },
+  addBtn: {
+    backgroundColor: '#333',
+    paddingBottom: 6,
+    paddingTop: 6,
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginBottom: 10,
+  },
+  addBtnText: {
+    fontSize: 14,
+    color: '#fff',
+  },
+  item: {
+      borderBottomWidth: 1,
+      borderBottomColor: '#365899',
+      paddingTop: 5,
+      marginBottom: 5,
+      paddingBottom: 1,
   },
 });
